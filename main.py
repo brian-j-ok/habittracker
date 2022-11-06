30e7f222e5682ad2204236ab0f449f270c690fee
Brian O'Kelly <brianjrok@gmail.com>
2022-11-06 13:05:55 -0700

    set up post request to create new user on Pixela

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "fjedlajfldwjlfjawldj",
+    "username": "brianjrok2531",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
